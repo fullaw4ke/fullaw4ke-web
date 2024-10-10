641191e971d2480f891020fa3a2c6daa8652ebf6
fullaw4ke <josemilane1322@gmail.com>
2024-10-10 17:51:43 -0400

    v1.1

---
+++ _astro/hoisted.fyIz0fhm.js
@@ -0,0 +1,1 @@
+import{a as t}from"./hoisted.XRwbKKUJ.js";import"./hoisted.8ps2ZCTu.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
