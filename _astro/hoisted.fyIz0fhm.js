1522cbebbeaab85a8d03a7e09f3760eeea0cacf5
fullaw4ke <josemilane1322@gmail.com>
2024-10-13 14:00:40 -0400

    fix Problems

--- _astro/hoisted.fyIz0fhm.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./hoisted.XRwbKKUJ.js";import"./hoisted.8ps2ZCTu.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
